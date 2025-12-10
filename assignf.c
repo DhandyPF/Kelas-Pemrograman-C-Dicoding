5677fcdbea431a026eaeb8403178b5918d53d9c4
DhandyPF <151412591+DhandyPF@users.noreply.github.com>
2025-12-10 22:48:56 +0700

    Add files via upload

---
+++ assignf.c
@@ -0,0 +1,17 @@
+/* File : assignf.c */
+/* Penulis : Dhandy, email dhandy.fahruddin@mhs.unsoed.ac.id */
+/* Mengisi variabel i yg bertype in dengan nilai 5 dan menuliskannya */
+#include <stdio.h>
+
+int main() {
+    /* Kamus */
+    float f = 0.555;
+    float x = 1.5E3;
+
+    /* Algoritma */
+    printf("Ini Nilai f : %f \n", f);
+    printf("Ini Nilai f : %5.2f \n", f);
+    printf("Ini Nilai x : %10.2f \n", x);
+    
+    return 0;
+}
