3dd53881ca324d9419a2afcb6410859b1040cf5f
DhandyPF <151412591+DhandyPF@users.noreply.github.com>
2025-12-12 19:33:06 +0700

    Add files via upload

---
+++ bacachar.c
@@ -0,0 +1,21 @@
+/* File : bacachar.c */
+/* Penulis : Dhandy, email dhandy.fahruddin@mhs.unsoed.ac.id */
+/* Deskripsi : */
+#include <stdio.h>
+
+int main() {
+    /* Kamus */
+    char cc;
+    char c;
+
+    /* Algoritma */
+    printf ("Hello\n");
+    printf("Baca 1 Karakter Saja : ");
+    scanf ("%c ", &cc);
+    printf ("%c\n", cc);
+    printf("Baca 1 Karakter Saja : ");
+    scanf ("%d ", &c); /*ketikkan angka 65 */
+    printf ("%d\n", c);
+
+    return 0;
+}
