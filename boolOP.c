9f197f962fa4fd3576afbc0b501e78bd50c29895
DhandyPF <151412591+DhandyPF@users.noreply.github.com>
2025-12-14 23:13:30 +0700

    Add files via upload

--- boolOP.c
+++ boolOP.c
@@ -11,16 +11,16 @@ int main() {
     int FALSE = 0;
 
     /* Algoritma  */
-    printf("Hasil Dari TRUE && TRUE = %d \n", TRUE && TRUE);
-    printf("Hasil Dari TRUE && FALSE = %d \n", TRUE && FALSE);
-    printf("Hasil Dari FALSE && TRUE = %d \n", FALSE && TRUE);
-    printf("Hasil Dari FALSE && FALSE = %d \n", FALSE && FALSE);
-    printf("Hasil Dari TRUE || TRUE = %d \n", TRUE || TRUE);
-    printf("Hasil Dari TRUE || FALSE = %d \n", TRUE || FALSE);
-    printf("Hasil Dari FALSE || TRUE = %d \n", FALSE || TRUE);
-    printf("Hasil Dari FALSE || FALSE = %d \n", FALSE || FALSE);
-    printf("Hasil Dari !TRUE = %d \n", !TRUE);
-    printf("Hasil Dari !FALSE = %d \n", !FALSE);
+    printf("Hasil Dari TRUE && TRUE : %d \n", TRUE && TRUE);
+    printf("Hasil Dari TRUE && FALSE : %d \n", TRUE && FALSE);
+    printf("Hasil Dari FALSE && TRUE : %d \n", FALSE && TRUE);
+    printf("Hasil Dari FALSE && FALSE : %d \n", FALSE && FALSE);
+    printf("Hasil Dari TRUE || TRUE : %d \n", TRUE || TRUE);
+    printf("Hasil Dari TRUE || FALSE : %d \n", TRUE || FALSE);
+    printf("Hasil Dari FALSE || TRUE : %d \n", FALSE || TRUE);
+    printf("Hasil Dari FALSE || FALSE : %d \n", FALSE || FALSE);
+    printf("Hasil Dari !TRUE : %d \n", !TRUE);
+    printf("Hasil Dari !FALSE : %d \n", !FALSE);
 
     return 0;
 }
---
+++ doWHILE.c
@@ -0,0 +1,18 @@
+/* File : doWHILE.c */
+/* Penulis : Dhandy, email dhandy.fahruddin@mhs.unsoed.ac.id */
+/* Deskripsi : */
+/* print “Saya senang” sebanyak 3 kali */
+#include <stdio.h>
+
+int main() {
+    /* Kamus : */
+    int i = 0;
+
+    /* Algoritma */
+    do {
+        printf("Saya Senang Belajar Bahasa C \n");
+        i++;
+    } while (i < 5);
+    
+    return 0;
+}
