bd660808b21a17e217b5caa672aaf586d581e77f
DhandyPF <151412591+DhandyPF@users.noreply.github.com>
2025-12-16 20:33:37 +0700

    Add files via upload

---
+++ for.c
@@ -0,0 +1,17 @@
+/* File : for.c */
+/* Penulis : Dhandy, email dhandy.fahruddin@mhs.unsoed.ac.id */
+/* Deskripsi :                                                   
+/* print “Saya senang” sebanyak 3 kali */
+#include <stdio.h>
+
+int main() {
+    /* Kamus : */
+    int i;
+
+    /* Algoritma */
+    for (int i = 0; i < 3; i++) {
+        printf("Saya Senang Belajar Bahasa C \n");
+    }
+
+    return 0;
+}
