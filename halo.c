20e245f4ad27d0cb8d74b4dedb2d20701253392f
DhandyPF <151412591+DhandyPF@users.noreply.github.com>
2025-12-18 15:17:30 +0700

    Add files via upload

---
+++ halo.c
@@ -0,0 +1,9 @@
+/* File : halo.c */
+/* Penulis : Dhandy, email dhandy.fahruddin@mhs.unsoed.ac.id */
+/* Program menuliskan “Halo Dunia! ” dan “end-of-line” (ganti baris) */
+#include <stdio.h>
+
+void main() {
+    /* Badan Program */
+    printf("Halo Dunia!");
+}
