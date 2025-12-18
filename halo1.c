d42e6b82dacc2f9804d8d603d8ff36a0d667d5c7
DhandyPF <151412591+DhandyPF@users.noreply.github.com>
2025-12-18 15:28:10 +0700

    Add files via upload

---
+++ halo1.c
@@ -0,0 +1,12 @@
+/* File : halo1.c */
+/* Penulis : Dhandy, email dhandy.fahruddin@mhs.unsoed.ac.id */
+/* Program untuk menuliskan Halo Dunia! ke layar */
+#include <stdio.h>
+
+int main() {
+    /* KAMUS */
+
+    /* ALGORITMA */
+    printf("Halo Dunia!");
+    return 0;
+}
