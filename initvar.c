ceb9aa960c13639df1108e65c69c6bd2fbf084a2
DhandyPF <151412591+DhandyPF@users.noreply.github.com>
2025-12-24 07:55:58 +0700

    Add files via upload

---
+++ initvar.c
@@ -0,0 +1,12 @@
+/* File : initvar.c */
+/* Penulis : Dhandy, email dhandy.fahruddin@mhs.unsoed.ac.id */
+/* Mengisi variabel i yg bertype in dengan nilai 5 dan menuliskannya */
+#include <stdio.h>
+
+int main() {
+    /* Kamus */
+    int i = 5; /* deklarasi dan inisialisasi nilai variabel i dengan 5 */
+    /* Algoritma */
+    printf ("Ini nilai i : %d \n", i);/*untuk mengamati dampak inisialisasi*/
+    return 0;
+}
