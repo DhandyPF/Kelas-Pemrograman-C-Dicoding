bdeccae7f930bc83b0beced5d1680f05a4b6defd
DhandyPF <151412591+DhandyPF@users.noreply.github.com>
2026-01-09 09:34:34 +0700

    Add files via upload

---
+++ WHILEdo.c
@@ -0,0 +1,18 @@
+/* File : WHILEdo.c */
+/* Penulis : Dhandy, email dhandy.fahruddin@mhs.unsoed.ac.id */
+/* Deskripsi : */
+/* print “Saya senang” sebanyak 3 kali */
+#include <stdio.h>
+
+int main() {
+    /* Kamus : */
+    int i = 0;
+
+    /* Algoritma */
+    while (i < 3) { /* program diulang selama nilai i < 3 */
+        printf("Saya Senang Belajar Bahasa C \n");
+        i++;
+    }
+    
+    return 0;
+}
